96e53cc211ec78c93081600daeecb6be1757f5ba
teawhy1230 <mmttt202@gmail.com>
2020-04-13 14:04:37 +0900

    Load elf from disk and jump to entry point

---
+++ hello_elf/start.c
@@ -0,0 +1,31 @@
+#define VRAM 0xb8000
+#define BG_COLOR 0x1f
+
+
+char msg[] = "Hello from ELF";
+
+
+void initbg() {
+    int i;
+    short *now = (short *)VRAM;
+    for (i = 0; i < 2000; i++) {
+        *now = BG_COLOR << 8;
+        now++;
+    }
+}
+
+
+void print(char *msg, int n) {
+    int i;
+    char *now = (char *)VRAM;
+    for (i = 0; i < n; i++) {
+        *now = msg[i];
+        now += 2;
+    }
+}
+
+
+void _start() {
+    initbg();
+    print(msg, sizeof(msg));
+}
